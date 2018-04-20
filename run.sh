ad409c71dcc81a7d02d48c9c5d30412023ff0285
Andrey Kolotiy <andrew.k.911@gmail.com>
2018-04-20 13:16:04 +0300

    Change to tar from zip input

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-unzip data/ldn_data.zip -d  /tmp/unzipped
+mkdir /tmp/unzipped
+tar xfv data/ldn_data.tar  -C  /tmp/unzipped
 gdal_calc.py -A /tmp/unzipped/Ukraine_LPD.tif -B /tmp/unzipped/Ukraine_map_7_cl_trans_00_16_no_city.tif --outfile=/tmp/unzipped/main_degrad.tif --calc="B*(A==1)+0*(A!=1)"
 gdal_calc.py -A /tmp/unzipped/Ukraine_LPD.tif -B /tmp/unzipped/Ukraine_map_7_cl_trans_00_16_no_city.tif --outfile=/tmp/unzipped/moderate_degrad.tif --calc="B*(A==2)+0*(A!=2)"
 :> data/count.txt
